7df6aa00eaaf053c13a8a6803a9ae43a5ebdf3e4
DestELYK <DestELYK@users.noreply.github.com>
2023-04-13 19:36:10 -0400

    Creating posts with Ajax

---
+++ src/static/functions.js
@@ -0,0 +1,9 @@
+console.log('hello world 2')
+
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
