66d1b44fdcfa18a7208f8fd616e38a0651d3f704
DestELYK <DestELYK@users.noreply.github.com>
2023-04-13 17:53:12 -0400

    Like Button with Ajax

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse
 
 # Create your views here.
 
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
 def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
@@ -22,10 +25,24 @@ def load_post_data_view(request, num_posts):
             'title': obj.title,
             'body': obj.body,
             'liked': True if request.user in obj.liked.all() else False,
+            'count': obj.like_count,
             'author': obj.author.user.username,
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
+
+def like_unlike_post(request):
+    if is_ajax(request):
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
